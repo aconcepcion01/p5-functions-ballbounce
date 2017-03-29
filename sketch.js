d8035d2d7c9a4ddabd8df842200d73484f069ef3
amani concepcion <aconcepcion@wlapcs.org>
2017-03-29 15:51:44 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -10,17 +10,12 @@ function setup(){
 
 function draw(){
     background(0);
+   display();
+   move();
 
-    //display ball
-    stroke(255);
-    strokeWeight(4);
-    fill(50,250,50);
-    ellipse(x, y, 50, 50);
 
     
-    //make ball move
-    y = y + speedy;
-    x = x + speedx;
+
     
     //ball changes direction when it hits wall
     if(x>600 || x<0){
@@ -28,5 +23,18 @@ function draw(){
     }
      if(y>400 || y<0){
         speedy = -speedy;
+    }
+    function display(){
+     //display ball
+    stroke(255);
+    strokeWeight(4);
+    fill(50,250,50);
+    ellipse(x, y, 50, 50);
+    }
+    
+    function move(){
+   //make ball move
+    y = y + speedy;
+    x = x + speedx;
     }
 }
